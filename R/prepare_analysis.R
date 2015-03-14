5b59cdce35622c7ef5d3da9378fa8b47d4fba361
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-03-14 13:56:31 +0100

    check the sanity of the arguments

--- R/prepare_analysis.R
+++ R/prepare_analysis.R
@@ -1,8 +1,10 @@
 #' Prepare all datasets and a to do list of models
 #' @inheritParams prepare_analysis_dataset
 #' @export
-#' @importFrom n2khelper list_files_git
+#' @importFrom n2khelper list_files_git check_single_strictly_positive_integer
 prepare_analysis <- function(min.observation = 100){
+  min.observation <- check_single_strictly_positive_integer(min.observation)
+  
   rawdata.files <- list_files_git(path = "abv")
   species <- as.integer(gsub("\\.txt$", "", rawdata.files))
   sha <- sapply(species, prepare_analysis_dataset, min.observation = min.observation)
