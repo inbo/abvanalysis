548d2fa40d9b401a3b410be41bdf2e9ab1552c39
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-04-26 21:11:44 +0200

    add connect_raw() and connect_attribute()

--- R/connect_source.R
+++ R/connect_source.R
@@ -5,3 +5,32 @@
 connect_source <- function(result.channel){
   odbc_connect(data.source.name = "Source data ABV", channel = result.channel)
 }
+
+
+#' Make a git connection to the attribute
+#' @export
+#' @inheritParams connect_source
+#' @inheritParams n2khelper::git_connection
+#' @importFrom n2khelper git_connect
+connect_attribute <- function(result.channel, username, password){
+  git_connect(
+    data.source.name = "Attributes ABV", 
+    channel = result.channel,
+    username = username,
+    password = password
+  )
+}
+
+#' Make a git connection to the raw data
+#' @export
+#' @inheritParams connect_source
+#' @inheritParams n2khelper::git_connection
+#' @importFrom n2khelper git_connect
+connect_raw <- function(result.channel, username, password){
+  git_connect(
+    data.source.name = "Raw data ABV", 
+    channel = result.channel,
+    username = username,
+    password = password
+  )
+}
