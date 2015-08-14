5c3ffa784bfd7d05df025d718bfae57158997ed5
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-14 11:26:23 +0200

    update code style on connect_attribute() and connect_raw()

--- R/connect_source.R
+++ R/connect_source.R
@@ -10,12 +10,17 @@ connect_source <- function(result.channel){
 #' Make a git connection to the attribute
 #' @export
 #' @inheritParams connect_source
-#' @param username The username for a https connection. Leave default for a ssh connection.
+#' @param username The username for a https connection. Leave default for a ssh
+#'    connection.
 #' @param password The password for the connection.
 #' @importFrom n2khelper git_connect
-connect_attribute <- function(result.channel, username = character(0), password = character(0)){
+connect_attribute <- function(
+  result.channel,
+  username = character(0),
+  password = character(0)
+){
   git_connect(
-    data.source.name = "Attributes ABV", 
+    data.source.name = "Attributes ABV",
     channel = result.channel,
     type = ifelse(length(username) == 0, "ssh", "https"),
     username = username,
@@ -28,9 +33,13 @@ connect_attribute <- function(result.channel, username = character(0), password 
 #' @inheritParams connect_source
 #' @inheritParams connect_attribute
 #' @importFrom n2khelper git_connect
-connect_raw <- function(result.channel, username = character(0), password = character(0)){
+connect_raw <- function(
+  result.channel,
+  username = character(0),
+  password = character(0)
+){
   git_connect(
-    data.source.name = "Raw data ABV", 
+    data.source.name = "Raw data ABV",
     channel = result.channel,
     type = ifelse(length(username) == 0, "ssh", "https"),
     username = username,
