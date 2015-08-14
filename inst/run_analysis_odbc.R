26cf178e14e4bdbba551d0b520c735b37d2f4643
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-14 10:58:58 +0200

    update code style for inst/run_analysis_odbc.R

--- inst/run_analysis_odbc.R
+++ inst/run_analysis_odbc.R
@@ -9,19 +9,19 @@ library(abvanalysis)
 
 result.channel <- n2khelper::connect_result()
 raw.connection <- connect_raw(
-  result.channel = result.channel, 
-  username = username, 
+  result.channel = result.channel,
+  username = username,
   password = password
 )
 scheme.id <- scheme_id(result.channel)
 prepare_dataset(
-  result.channel = result.channel, 
-  source.channel = connect_source(result.channel), 
+  result.channel = result.channel,
+  source.channel = connect_source(result.channel),
   attribute.connection = connect_attribute(
-    result.channel = result.channel, 
-    username = username, 
+    result.channel = result.channel,
+    username = username,
     password = password
-  ), 
-  raw.connection = raw.connection, 
+  ),
+  raw.connection = raw.connection,
   scheme.id = scheme.id
 )
