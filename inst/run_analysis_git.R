e2b3f5730a9c1ac2ce58f39243b56295e6676f44
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-08-14 10:58:33 +0200

    update code style for inst/run_analysis_git.R

--- inst/run_analysis_git.R
+++ inst/run_analysis_git.R
@@ -1,33 +1,33 @@
 library(abvanalysis)
 raw.connection <- n2khelper::git_connection(
-  repo.path = "~/n2k/ssh/rawdata",
+  repo.path = "~/n2k/ssh/rawdata", #nolint
   local.path = "abv",
-  key = "~/.ssh/id_rsa_n2kreadonly"
+  key = "~/.ssh/id_rsa_n2kreadonly" #nolint
 )
 prepare_analysis(
   raw.connection = raw.connection,
-  analysis.path = "~/analysis/abv"
+  analysis.path = "~/analysis/abv" #nolint
 )
 
 library(optimx)
 library(n2kanalysis)
 fit_every_model(
-  path = "~/analysis/abv", 
-  status = c("new", "waiting"), 
+  path = "~/analysis/abv", #nolint
+  status = c("new", "waiting"),
   n.cluster = parallel::detectCores() - 1
 )
 fit_every_model(
-  path = "~/analysis/abv", 
-  status = c("new", "waiting"), 
+  path = "~/analysis/abv", #nolint
+  status = c("new", "waiting"),
   n.cluster = 1
 )
-status("~/analysis/abv")
+status("~/analysis/abv") #nolint
 
 library(n2kanalysis)
 result <- get_result(
-  x = "~/analysis/abv", 
+  x = "~/analysis/abv", #nolint
   datasource.id = result_datasource_id(result.channel = result.channel),
-  keep.fingerprint = FALSE, 
+  keep.fingerprint = FALSE,
   n.cluster = parallel::detectCores() - 1
 )
-save(result, file = "~/analysis/output/abv.rda")
+save(result, file = "~/analysis/output/abv.rda") #nolint
