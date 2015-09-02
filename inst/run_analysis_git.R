189afa58a960d038b678806486748f06458328a4
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-09-02 17:44:45 +0200

    INLA models run in parallel

--- inst/run_analysis_git.R
+++ inst/run_analysis_git.R
@@ -16,7 +16,7 @@ library(n2kanalysis)
 fit_every_model(
   path = "~/analysis/abv", #nolint
   status = c("new", "waiting"),
-  n.cluster = parallel::detectCores() - 1
+  n.cluster = 1
 )
 fit_every_model(
   path = "~/analysis/abv", #nolint
