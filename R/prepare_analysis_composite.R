5932420566657ae0b27ff3dc6d35d110c0d0cb8f
Thierry onkelinx <thierry.onkelinx@inbo.be>
2015-09-18 09:30:04 +0200

    add extractor to the n2kComposite object

--- R/prepare_analysis_composite.R
+++ R/prepare_analysis_composite.R
@@ -27,6 +27,65 @@ prepare_analysis_composite <- function(dataset, raw.connection, analysis.path){
   dataset$ParentStatusFingerprint <- dataset$StatusFingerprint
   dataset$ParentStatus <- dataset$Status
 
+  extractor <- switch(
+    dataset$Covariate[1],
+    "fCycle" = function(model){
+      if (!require(dplyr)) {
+        stop("'dplyr' is required")
+      }
+      if (!require(INLA)) {
+        stop("'INLA' is required")
+      }
+      parameter <- model$summary.lincomb.derived %>%
+        add_rownames("Value") %>%
+        mutate_(Variance = ~sd ^ 2) %>%
+        select_(~Value, Estimate = ~mean, ~Variance) %>%
+        arrange_(~Value)
+      reference <- parameter %>%
+        head(1)
+      parameter %>%
+        mutate_(Estimate = ~Estimate - reference$Estimate)
+    },
+    "cYear" = function(model){
+      if (!require(dplyr)) {
+        stop("'dplyr' is required")
+      }
+      if (!require(INLA)) {
+        stop("'INLA' is required")
+      }
+      parameter <- model$summary.lincomb.derived %>%
+        add_rownames("Value") %>%
+        mutate_(Variance = ~sd ^ 2) %>%
+        select_(~Value, Estimate = ~mean, ~Variance)
+      reference <- parameter %>%
+        filter_(~Value != "Trend") %>%
+        mutate_(Year = ~as.numeric(Value)) %>%
+        arrange_(~Year) %>%
+        head(1)
+      parameter %>%
+        mutate_(
+          Estimate = ~Estimate - ifelse(Value == "Trend", 0, reference$Estimate)
+        )
+    },
+    "fYear" = function(model){
+      if (!require(dplyr)) {
+        stop("'dplyr' is required")
+      }
+      if (!require(INLA)) {
+        stop("'INLA' is required")
+      }
+      parameter <- model$summary.lincomb.derived %>%
+        add_rownames("Value") %>%
+        mutate_(Variance = ~sd ^ 2) %>%
+        select_(~Value, Estimate = ~mean, ~Variance)
+      reference <- parameter %>%
+        mutate_(Year = ~as.numeric(Value)) %>%
+        arrange_(~Year) %>%
+        head(1)
+      parameter %>%
+        mutate_(Estimate = ~Estimate - reference$Estimate)
+    }
+  )
 
   analysis <- n2k_composite(
     parent.status = dataset[
@@ -41,6 +100,7 @@ prepare_analysis_composite <- function(dataset, raw.connection, analysis.path){
     formula = paste("~", dataset$Covariate[1]),
     first.imported.year = first.year,
     last.imported.year = last.year,
+    extractor = extractor,
     analysis.date = max(dataset$AnalysisDate)
   )
   file.fingerprint <- get_file_fingerprint(analysis)
