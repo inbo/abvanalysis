fa5eeae9cef389ce0a9eddfa7ba1b087010a6400
ThierryO <Thierry.Onkelinx@inbo.be>
2016-04-26 10:56:34 +0200

    allow to switch between ssh and https for rawdata git repository

--- inst/run_analysis_git.R
+++ inst/run_analysis_git.R
@@ -1,12 +1,22 @@
+protocol <- "ssh"
 library(abvanalysis)
-raw.connection <- n2khelper::git_connection(
-  repo.path = "~/n2k/https/rawdata", #nolint
-  local.path = "abv",
-  username = username,
-  password = password,
-  commit.user = "abvanalysis",
-  commit.email = "bmk@inbo.be"
-)
+if (protocol == "ssh") {
+  raw.connection <- n2khelper::git_connection(
+    repo.path = "~/n2k/ssh/rawdata", #nolint
+    local.path = "abv",
+    commit.user = "abvanalysis",
+    commit.email = "bmk@inbo.be"
+  )
+} else {
+  raw.connection <- n2khelper::git_connection(
+    repo.path = "~/n2k/https/rawdata", #nolint
+    local.path = "abv",
+    username = username,
+    password = password,
+    commit.user = "abvanalysis",
+    commit.email = "bmk@inbo.be"
+  )
+}
 prepare_analysis(
   raw.connection = raw.connection,
   analysis.path = "~/analysis/abv" #nolint
