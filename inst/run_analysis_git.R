82896c1cab6fc12122c11fc4e85e1cf71ca3dd5b
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2019-12-02 09:55:17 +0100

    update script to import the data and prepare the analysis

--- inst/run_analysis_git.R
+++ inst/run_analysis_git.R
@@ -1,63 +1,33 @@
-protocol <- "ssh"
 library(abvanalysis)
-if (protocol == "ssh") {
-  raw.connection <- n2khelper::git_connection(
-    repo.path = "~/n2k/ssh/rawdata", #nolint
-    local.path = "abv",
-    commit.user = "abvanalysis",
-    commit.email = "bmk@inbo.be"
+repo <- git2rdata::repository("~/n2k/abv")
+result <- n2khelper::connect_result(
+  username = Sys.getenv(
+    "N2KRESULT_USERNAME",
+    keyringr::decrypt_gk_pw("db n2kresult n2k_analysis user")
+  ),
+  password = Sys.getenv(
+    "N2KRESULT_PASSWORD",
+    keyringr::decrypt_gk_pw("db n2kresult n2k_analysis pwd")
   )
-} else {
-  raw.connection <- n2khelper::git_connection(
-    repo.path = "~/n2k/https/rawdata", #nolint
-    local.path = "abv",
-    username = username,
-    password = password,
-    commit.user = "abvanalysis",
-    commit.email = "bmk@inbo.be"
+)
+dbplyr::src_dbi(
+  RPostgreSQL::dbConnect(
+    RPostgreSQL::PostgreSQL(),
+    host = "localhost",
+    port = Sys.getenv("meetnetten_port"),
+    dbname = "meetnetten",
+    user = Sys.getenv("meetnetten_user"),
+    password = Sys.getenv("meetnetten_password")
   )
-}
-prepare_analysis(
-  raw.connection = raw.connection,
-  analysis.path = "/media/thierry_onkelinx/CE74C8F474C8E077/analysis/abv" #nolint
+) -> origin
+prepare_dataset(
+  result = result, origin = origin, repo = repo,
+  scheme.id = scheme_id(result.channel = result)
 )
 
-library(n2kanalysis)
-fit_every_model(
-  path = "/media/thierry_onkelinx/CE74C8F474C8E077/analysis/abv", #nolint
-  status = c("new", "waiting"),
-  n.cluster = 7
-)
-fit_every_model(
-  path = "~/analysis/abv", #nolint
-  status = c("new", "waiting"),
-  n.cluster = 1
-)
-current.status <- status("~/analysis/abv") #nolint
-table(current.status$Status)
-
-library(n2kanalysis)
-result.channel <- n2khelper::connect_result(
-  username = Sys.getenv("N2KRESULT_USERNAME"),
-  password = Sys.getenv("N2KRESULT_PASSWORD")
-)
-datasource.id <- abvanalysis::result_datasource_id(
-  result.channel = result.channel
-)
-result <- get_result(
-  x = "/media/thierry_onkelinx/CE74C8F474C8E077/analysis/abv", #nolint
-  datasource.id = datasource.id,
-  keep.fingerprint = FALSE
-)
-save(result, file = "/media/thierry_onkelinx/CE74C8F474C8E077/analysis/output/abv.rda")
-result <- get_result(
-  x = "/media/thierry_onkelinx/CE74C8F474C8E077/analysis/abv", #nolint
-  datasource.id = datasource.id,
-  keep.fingerprint = FALSE,
-  n.cluster = parallel::detectCores() - 1
-)
-save(result, file = "~/analysis/output/abv.rda") #nolint
-#nolint
-
-get_result("/media/thierry_onkelinx/CE74C8F474C8E077/analysis/abv/068e6562540bd354e874ffd53c00d6897c97eacc.rda", datasource.id = datasource.id,
-  keep.fingerprint = FALSE)
+library(abvanalysis)
+options(sha1PackageVersion = "0.6.22")
+repo <- git2rdata::repository("~/n2k/abv")
+base <- aws.s3::get_bucket("n2kmonitoring", max = 1)
+script <- prepare_analysis(repo = repo, base = base, project = "abv")
+writeLines(script, "~/n2kanalysis/abv.sh")
