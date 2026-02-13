f6a952726fc7688871e90468eef4996ed979b31d
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2026-02-13 11:24:00 +0100

    ✨ Update analysis script

--- inst/analysis_local.R
+++ inst/analysis_local.R
@@ -1,4 +1,5 @@
 library(abvanalysis)
+library(n2kanalysis)
 repo <- git2rdata::repository(file.path("~", "n2k", "abv"))
 origin <- odbc::dbConnect(
   odbc::odbc(), driver = "ODBC Driver 18 for SQL Server",
@@ -7,16 +8,20 @@ origin <- odbc::dbConnect(
   pwd = keyring::key_get("meetnetten", username = "bmkreader")
 ) # sql server
 project <- "abv"
-base <- aws.s3::get_bucket(Sys.getenv("N2KBUCKET"), prefix = project, max = 1)
 
 prepare_dataset(
   origin = origin, repo = repo, end_date = Sys.time(), verbose = TRUE,
   push = FALSE, strict = FALSE, db_scheme = "staging_Meetnetten"
 )
 
+connect_inbo_s3()
+base <- aws.s3::get_bucket(Sys.getenv("N2KBUCKET"), prefix = project, max = 1)
 prepare_analysis(
-  repo = repo, base = base, project = project, docker = "inbobmk/rn2k:dev-0.10",
-  dependencies = c("inbo/n2khelper@v0.5.0", "inbo/n2kanalysis@v0.4.0")
+  repo = repo,
+  base = base,
+  project = project,
+  docker = "inbobmk/rn2k:dev-0.10",
+  dependencies = c("inbo/n2khelper@v0.5.0", "inbo/n2kanalysis@v0.4.1")
 )
 
 read_manifest(base = base, project = project) |>
